9e89fcb97e7dbe916b6fed3544333254c6b96487
Jiaqi (Alex) Wang <jwang98@stanford.edu>
2017-11-01 22:22:47 -0700

    fixed scraper

--- Data scraping/data_scraper.py
+++ Data scraping/data_scraper.py
@@ -1,4 +1,4 @@
-from py_genius import Genius
+import pylyrics3
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -8,7 +8,7 @@ def getArtists():
     with open('artists.txt') as f:
         return f.read().splitlines()
 
-
+'''
 def getIds(gen):
     ids_tosearch = []
     for artist in getArtists():
@@ -35,39 +35,18 @@ def lyrics_from_song_api_path(song_api_path):
   [h.extract() for h in html('script')]
   lyrics = html.find('div', class_='lyrics').get_text()
   return lyrics
+'''
 
 def scrape():
     with open('songs.csv','w',encoding='utf-8') as file:
         writer = csv.writer(file, delimiter = '|')
-        gen = Genius('thRsqBz-IHPVCj0IU6_VNpRjRs9o2HGNxN_WAAoXCyOJPuRbhyb0MSJGNFcTnnlQ')
-        ids = list(map(int, getArtists()))
-        for id in ids:
-            response = gen.get_artist(id)
-            name = response['response']['artist']['name']
-            title = ''
-            k = 0
-            num_songs = 0
-            lst = []
-            response = gen.get_artist_songs(id)
-            print(response)
-            while True:
-                page_num = 1
-                print(k)
-                print(name)
-                song = response['response']['songs'][k]
-                outcome = song['primary_artist']['name']
-                title = song['title']
-                if name == outcome and song['title'] == song['title_with_featured']:
-                    newRow = [name, title, lyrics_from_song_api_path(song['api_path']).replace('\n', ' ')]
-                    print(newRow)
-                    writer.writerow(newRow)
-                    if num_songs >= 30:
-                        break
-                    num_songs+=1
-                k+=1
-                if k == 18:
-                    response = gen.get_artist_songs(id, page=page_num+1)
-                    k = 0
+        artists = getArtists()
+        for art in artists:
+            lyrics = pylyrics3.get_artist_lyrics(art)
+            for key, val in lyrics:
+                newRow = [art, key, val]
+                print(newRow)
+                writer.writerow(newRow)
 
 
 data = pd.read_csv('songs.csv',delimiter='|')
