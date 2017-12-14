ba2054fcda39b006ef02552a73eb86852ebf757e
Jiaqi (Alex) Wang <jwang98@stanford.edu>
2017-12-14 02:34:08 -0800

    finalized classifiers

--- nn.py
+++ nn.py
@@ -2,22 +2,20 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-train_data = pd.read_csv('../train_10-1000_binary.csv')
-dev_data = pd.read_csv('../dev_10-1000_binary.csv')
-test_data = pd.read_csv('../test_10-1000_binary.csv')
+train_data = pd.read_csv('../train_10-1000_norm.csv')
+dev_data = pd.read_csv('../dev_10-1000_norm.csv')
+test_data = pd.read_csv('../test_10-1000_norm.csv')
 
-learning_rate = 0.01
-num_steps = 100
+lst = range(0, 1000, 50)
+
+learning_rate = 0.1
+num_steps = 1000
 display_step = 10
-# Network Parameters
-n_hidden_1 = 4685 # 1st layer number of neurons
-n_hidden_2 = 4685
-num_input = 4685 # MNIST data input (img shape: 28*28)
-num_classes = 12 # MNIST total classes (0-9 digits)
+
 
 
 def onehot(y):
-    ret = np.zeros((len(y),num_classes))
+    ret = np.zeros((len(y),12))
     for i in range(len(y)):
         num = y[i]
         ret[i,int(num)] = 1.0
@@ -30,6 +28,11 @@ dev_y = onehot(np.array(dev_data['0'].values))
 test_x = np.array(test_data.iloc[:, 1:])
 test_y = onehot(np.array(test_data['0'].values))
 
+num_input = len(train_x[0,:])
+n_hidden_1 = 360
+#n_hidden_2 = 120
+num_classes = 12
+
 # tf Graph input
 X = tf.placeholder("float", [None, num_input])
 Y = tf.placeholder("float", [None, num_classes])
@@ -37,12 +40,12 @@ Y = tf.placeholder("float", [None, num_classes])
 # Store layers weight & bias
 weights = {
     'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
+    #'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_1, num_classes]))
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    #'b2': tf.Variable(tf.random_normal([n_hidden_2])),
     'out': tf.Variable(tf.random_normal([num_classes]))
 }
 
@@ -50,37 +53,35 @@ biases = {
 # Create model
 def neural_net(x):
     # Hidden fully connected layer with 256 neurons
-    layer_1 = (tf.add(tf.matmul(x, weights['h1']), biases['b1']))
-    layer_2 = (tf.add(tf.matmul(layer_1,weights['h2']), biases['b2']))
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['h1']), biases['b1']))
+    #layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1,weights['h2']), biases['b2']))
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
     return out_layer
 
 # Construct model
 logits = neural_net(X)
 prediction = tf.nn.softmax(logits)
-
 # Define loss and optimizer
 loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=logits, labels=Y) + 0.001 * tf.nn.l2_loss(weights['h1'])+ 0.001 * tf.nn.l2_loss(weights['h2'])+ 0.001 * tf.nn.l2_loss(weights['out'])
-                         +0.001 * tf.nn.l2_loss(biases['b1'])+ 0.001 * tf.nn.l2_loss(biases['b2'])+ 0.001 * tf.nn.l2_loss(biases['out']))
+    logits=logits, labels=Y) + 0.05 * tf.nn.l2_loss(weights['out']) + 0.05 * tf.nn.l2_loss(weights['h1']) + 0.05 * tf.nn.l2_loss(biases['b1'])# + 0.01 * tf.nn.l2_loss(weights['h2']) + 0.01 * tf.nn.l2_loss(biases['b2'])
+                         + 0.05 * tf.nn.l2_loss(biases['out']))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
 
 # Start training
 with tf.Session() as sess:
-
+    k = 200
     # Run the initializer
     sess.run(init)
 
     for step in range(1, num_steps+1):
-        inds = np.random.permutation(train_x.shape[0])[:200]
+        inds = np.random.permutation(train_x.shape[0])[:k]
         # Run optimization op (backprop)
         sess.run(train_op, feed_dict={X: train_x[inds,:], Y: train_y[inds,:]})
         if step % display_step == 0:
@@ -89,9 +90,32 @@ with tf.Session() as sess:
             print("Step " + str(step) + ", Minibatch Loss= " + \
                     "{:.4f}".format(loss) + ", Training Accuracy= " + \
                     "{:.3f}".format(acc))
+            if loss < 1:
+                learning_rate = 0.01
+            elif loss < 5:
+                learning_rate = 0.05
+        if step in lst:
+            pred,dev_acc = sess.run([prediction,accuracy], feed_dict={X: dev_x, Y: dev_y})
+            top2 = np.argsort(pred)[:,[-1,-2]]
+            count = 0.0
+            for j in range(dev_y.shape[0]):
+                if np.argmax(dev_y[j,:]) in top2[j,:]:
+                    count+=1
+            top2_acc = count / dev_y.shape[0]
+            print("Dev Accuracy:", dev_acc)
+            print("Dev Top 2 Accuracy:", top2_acc)
+        if step > 350:
+            k = train_x.shape[0]
 
     print("Optimization Finished!")
 
     # Calculate accuracy for MNIST test images
-    print("Testing Accuracy:", sess.run(accuracy, feed_dict={X: dev_x, Y: dev_y}))
-    print("Testing Accuracy:", sess.run(accuracy, feed_dict={X: test_x, Y: test_y}))
+    pred, dev_acc = sess.run([prediction, accuracy], feed_dict={X: dev_x, Y: dev_y})
+    top2 = np.argsort(pred)[:, [-1, -2]]
+    count = 0.0
+    for j in range(dev_y.shape[0]):
+        if np.argmax(dev_y[j, :]) in top2[j, :]:
+            count += 1
+    top2_acc = count / dev_y.shape[0]
+    print("Dev Accuracy:", dev_acc)
+    print("Dev Top 2 Accuracy:", top2_acc)
--- onevrest.py
+++ onevrest.py
@@ -2,22 +2,25 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from matplotlib import pyplot as plt
 from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import plot_importance
+from sklearn.feature_selection import RFE
 import pandas as pd
 import numpy as np
 
-'''
-train_data = pd.read_csv('../norm_train_10-1000_regular.csv')
-dev_data = pd.read_csv('../norm_dev_10-1000_regular.csv')
-test_data = pd.read_csv('../norm_test_10-1000_regular.csv')
+
+
+train_data = pd.read_csv('../train_10-1000_norm.csv')
+dev_data = pd.read_csv('../dev_10-1000_norm.csv')
+test_data = pd.read_csv('../test_10-1000_norm.csv')
+train_data = pd.concat([train_data,dev_data])
 '''
 train_data = pd.read_csv('../train_10-1000_binary.csv')
 dev_data = pd.read_csv('../dev_10-1000_binary.csv')
 test_data = pd.read_csv('../test_10-1000_binary.csv')
-
+'''
 def onehot(y):
     ret = np.zeros((len(y),12))
     for i in range(len(y)):
@@ -25,20 +28,23 @@ def onehot(y):
         ret[i,int(num)] = 1.0
     return ret
 
+
 train_x = np.array(train_data.iloc[:, 1:])
 train_y = onehot(np.array(train_data['0'].values))
+t_y = np.array(train_data['0'].values)
 dev_x = np.array(dev_data.iloc[:, 1:])
 dev_y = np.array(dev_data['0'].values)
 test_x = np.array(test_data.iloc[:, 1:])
 test_y = np.array(test_data['0'].values)
 
-classifiers = [SVC(probability=True) for _ in range(12)]
+classifiers = [SVC(probability=True, C=1.5) for _ in range(12)]
 print('Training...')
 for i in range(12):
     print(i)
     classifiers[i].fit(train_x,train_y[:,i])
 
-print('Predicting...')
+
+print('Predicting dev...')
 predict_dev = [0.0 for _ in range(dev_y.shape[0])]
 for j in range(dev_y.shape[0]):
     pred = [cla.predict_proba([dev_x[j,:]])[0,1] for cla in classifiers]
@@ -48,8 +54,15 @@ print(predict_dev[:5])
 print(dev_y[:5])
 print(sum(predict_dev == dev_y) / float(len(dev_y)))
 
+'''
+confus = np.zeros((12,12))
+for i in range(len(dev_y)):
+    confus[int(dev_y[i]),int(predict_dev[i])] += 1
+print(confus)
+'''
 
-print('Predicting...')
+
+print('Predicting test...')
 predict_test = [0.0 for _ in range(test_y.shape[0])]
 for j in range(test_y.shape[0]):
     pred = [cla.predict_proba([test_x[j,:]])[0,1] for cla in classifiers]
@@ -58,3 +71,14 @@ for j in range(test_y.shape[0]):
 print(predict_test[:5])
 print(test_y[:5])
 print(sum(predict_test == test_y) / float(len(test_y)))
+
+print('Predicting train...')
+predict_train = [0.0 for _ in range(t_y.shape[0])]
+for j in range(t_y.shape[0]):
+    pred = [cla.predict_proba([train_x[j,:]])[0,1] for cla in classifiers]
+    predict_train[j] = np.argmax(pred)
+
+print(predict_train[:5])
+print(t_y[:5])
+print(sum(predict_train == t_y) / float(len(t_y)))
+
